3cdb1bbcb4b4c99da17588712697948a066fed49
Valentin Barthel <135817089+valentinb67@users.noreply.github.com>
2024-04-01 16:39:57 +0200

    Update scraping_festivals.py
    Ajout des commentaires

--- scraping_festivals.py
+++ scraping_festivals.py
@@ -3,10 +3,11 @@ from bs4 import BeautifulSoup
 import re
 import pandas as pd
 
+#Appel de la fonction get_url() depuis le script scraping_blogs
 from scraping_blogs import get_url
-
 print(get_url('https://www.smoothjazz.com/festivals'))
 
+#Création d'une fonction permettant de scraper toutes les pages contenant les informations à scrapper sur les festivals 
 def get_all_pages():
     urls = []
     page_number = 0
@@ -16,6 +17,7 @@ def get_all_pages():
         urls.append(url)
     return urls
 
+#Création d'une fonction permettant de scraper le nom, la ville, la région, le pays et la date des festivals
 def get_event_names(url):
     r = requests.get(url)
     soup = BeautifulSoup(r.content, features="lxml")
@@ -30,7 +32,8 @@ def get_event_names(url):
         event_details['Periode_Event'] = festival.find("td", headers="view-field-event-month-table-column", class_="views-field views-field-field-event-month").text.strip() if festival.find("td", headers="view-field-event-month-table-column", class_="views-field views-field-field-event-month") else ""
         data.append(event_details)
     return data
-
+    
+#Création d'une fonction afin d'appliquer la fonction pour obtenir les informations de tout les festivals pour chacunes des pages (get_all_pages)
 def scrape_all_events():
     urls = get_all_pages()
     all_events = []
@@ -39,10 +42,12 @@ def scrape_all_events():
         all_events.extend(events)
     return pd.DataFrame(all_events)
 
+#Création d'un dataframe  TableEvent conservant l'ensemble des informations scrapées
 TableEvent = scrape_all_events()
 
 TableEvent["Pays_Event"] = TableEvent["Pays_Event"].replace({"United States": "USA"})
 TableEvent
 
+#Téléchargement de la database TableEvent en csv dans le même folder que celui du script et des autres scripts
 nom_fichier = 'TableEvent.csv'
 TableEvent.to_csv(nom_fichier, index=False)
